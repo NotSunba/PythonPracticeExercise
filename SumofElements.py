0bb6380a77be77ecb15b1fc58507dfb0c0321306
NotSunba <antonsumbs45@gmail.com>
2025-04-26 17:24:37 +0300

    sum of all elements in a list

--- SumofElements.py
+++ SumofElements.py
@@ -1,4 +1,4 @@
 my_marks = input("Write your marks:")
 number = [int(mark) for mark in my_marks.split()] #changes your input from string to integers
-total = sum(number) #adds all the numbers in the list
+total = sum(number) #adds all the numbers in the list.
 print(f"The total of your marks is {total}")
